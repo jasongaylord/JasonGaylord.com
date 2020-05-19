9b0e7abe95447069ba40264a2dbb267ca3899e46
Jason Gaylord <Jason.Gaylord@biberk.com>
2020-05-19 14:27:43 -0400

    Updated clipboard functionality.

---
+++ assets/js/clipboard.js
@@ -0,0 +1,25 @@
+SpaacedOut.ready(function() {
+
+    var copy_elements = document.querySelectorAll("[data-copy-text]");
+    var copy_count;
+
+    for (copy_count = 0; copy_count < copy_elements.length; copy_count++) {
+        copy_elements.elements[i].addEventListener("click", async(event) => {
+            if (!navigator.clipboard) {
+                return;
+            }
+
+            try {
+                var copy_value = copy_elements.elements[i].getAttribute("data-copy-text");
+                await navigator.clipboard.writeText(copy_value);
+
+                event.target.dataset.clipboard = copy_value;
+                setTimeout(() => {
+                delete event.target.dataset.clipboard;
+                }, 1500); 
+            } catch (error) {
+                console.error("copy failed", error);
+            }
+        });
+    }
+});
